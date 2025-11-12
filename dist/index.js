4f031a38ebd4bfcf0e34dff312545878d5c7c4f6
Vlad Melnyk <vlad22523@gmail.com>
2025-11-12 16:01:08 +0200

    ts-1

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const userName = "Влад";
+const age = 20;
+const isStudent = true;
+console.log(`Привіт, мене звати ${userName}. Мені ${age} років. Студент: ${isStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const userName: string = "Влад";
+const age: number = 20;
+const isStudent: boolean = true;
+
+console.log(
+  `Привіт, мене звати ${userName}. Мені ${age} років. Студент: ${isStudent}`
+);
